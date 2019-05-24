18d3238814efd6dacd1001d7b32b7631323f7db4
Freddie Mungen <50964085+freddiejidc@users.noreply.github.com>
2019-05-24 00:10:28 -0400

    style for the header css and php

---
+++ header.php
@@ -0,0 +1,55 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="gobro.css">
+    <script src="main.js"></script>
+</head>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Login, Sign up Header</title>
+
+	<link rel="stylesheet" href="demo.css">
+	<link rel="stylesheet" href="header-login-signup.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<header class="header-login-signup">
+
+	<div class="header-limiter">
+
+		<h1><a href="#">Company<span>logo</span></a></h1>
+
+		<nav>
+			<a href="#">Home</a>
+			<a href="#" class="selected">Blog</a>
+			<a href="#">Pricing</a>
+		</nav>
+
+		<ul>
+			<li><a href="#">Login</a></li>
+			<li><a href="#">Sign up</a></li>
+		</ul>
+
+	</div>
+
+</header>
+
+
+</body>
+
+</html>
